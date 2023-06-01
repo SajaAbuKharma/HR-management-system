2d80ca9658f438eb3513cdfb3748275c32dd8942
sajaabukharma <sajaabukharma@gmail.com>
2023-06-01 15:03:37 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,133 +1,75 @@
 'use strict' ;
 
-let emp1 = { 
-    ID: getEmployeeID(), 
-    name: "Ghazi Samer", 
-    department: "Administration", 
-    level: "Senior", 
-    image: "", 
-} 
-emp1.salary = calculateEmployeeSalary(emp1.level); 
-let emp2 = { 
-    ID: getEmployeeID(), 
-    name: "Lana Ali", 
-    department: "Finance", 
-    level: "Senior", 
-    image: "", 
-} 
-emp2.salary = calculateEmployeeSalary(emp2.level); 
-let emp3 = { 
-    ID: getEmployeeID(), 
-    name: "Tamara Ayoub", 
-    department: "Marketing", 
-    level: "Senior", 
-    image: "", 
-} 
-emp3.salary = calculateEmployeeSalary(emp3.level); 
-let emp4 = { 
-    ID: getEmployeeID(), 
-    name: "Safi Walid", 
-    department: "Administration", 
-    level: "Mid-Senior", 
-    image: "", 
-} 
-emp4.salary = calculateEmployeeSalary(emp4.level); 
-let emp5 = { 
-    ID: getEmployeeID(), 
-    name: "Omar Zaid", 
-    department: "Development", 
-    level: "Senior", 
-    image: "", 
-} 
-emp5.salary = calculateEmployeeSalary(emp5.level); 
-let emp6 = { 
-    ID: getEmployeeID(), 
-    name: "Rana Saleh", 
-    department: "Development", 
-    level: "Junior", 
-    image: "", 
-} 
-emp6.salary = calculateEmployeeSalary(emp6.level); 
-let emp7 = { 
-    ID: getEmployeeID(), 
-    name: "Hadi Ahmed", 
-    department: "Finance", 
-    level: "Mid-Senior", 
-    image: "", 
-} 
-emp7.salary = calculateEmployeeSalary(emp7.level); 
- 
-function getEmployeeID() { 
-    // for testing 
-    let x = Math.ceil(Math.random() * (9999 - 1000)) + 1000; 
-    console.log(`id = ${x}`); 
- 
-    return Math.ceil(Math.random() * (9999 - 1000)) + 1000; 
-} 
- 
-function calculateEmployeeSalary(employeeLevel) { 
-    let salary = 0; 
-    let netSalary = 0; 
-    switch (employeeLevel) { 
-        case "Senior": 
-            salary = Math.ceil(Math.random() * (2000 - 1500)) + 1500; 
- 
-            // for testing 
-            console.log(salary); 
-            break; 
-        case "Mid-Senior": 
-            salary = Math.ceil(Math.random() * (1500 - 1000)) + 1000; 
- 
-            // for testing 
-            console.log(salary); 
-            break; 
-        case "Junior": 
-            salary = Math.ceil(Math.random() * (1000 - 500)) + 500; 
- 
-            // for testing 
-            console.log(salary); 
-            break; 
-        default: 
-            console.log("Invalid level"); 
-    } 
-    netSalary = salary - (salary * 7.5 / 100); 
-    return netSalary; 
-} 
- 
-function renderEmployeeInfo() { 
-    let employeeInfo = document.getElementById("employee-info"); 
-    employeeInfo.innerHTML = ` 
-    <h2>Employees Info:</h2><br> 
-    <p> 
-        Employee Name: ${emp1.name}<br> 
-        Employee Salary: ${emp1.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp2.name}<br> 
-        Employee Salary: ${emp2.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp3.name}<br> 
-        Employee Salary: ${emp3.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp4.name}<br> 
-        Employee Salary: ${emp4.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp5.name}<br> 
-        Employee Salary: ${emp5.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp6.name}<br> 
-        Employee Salary: ${emp6.salary} 
-    </p><br> 
-    <p> 
-        Employee Name: ${emp7.name}<br> 
-        Employee Salary: ${emp7.salary} 
-    </p> 
-` 
-} 
- 
-renderEmployeeInfo(); 
-renderEmployeeInfo
+
+                                       function Emp (id,name,departement,Level,img,salary){
+   this. EmlpoyeeId=id;
+   this. FullName=name;
+   this. Departement=departement;
+   this. level=Level;
+    this.image=img;
+    this.Salary=salary;
+}
+   
+//function
+                                               function generateEmployeeId() {
+    let employeeId;
+    const usedIds = new Set();
+  
+    do {
+
+      employeeId = Math.floor(Math.random() * 9000) + 1000;
+    } while (usedIds.has(employeeId));
+  
+    usedIds.add(employeeId);
+    return employeeId;
+  } 
+  
+  //function 
+                                              function calculateSalary(level){
+
+    let salaryRange={
+        Senior: { min: 1500, max: 2000 },
+    Midsenior: { min: 1000, max: 1500 },
+    Junior: { min: 500, max: 1000 },
+    }
+
+ let minSalary = salaryRange[level].min;
+  let maxSalary = salaryRange[level].max;
+  let randomSalary = Math.floor(minSalary + Math.random() * (maxSalary - minSalary + 1));
+  let netSalary = randomSalary - (randomSalary * 0.075); 
+  return netSalary;
+};
+
+/* new */
+
+let Ghazi=new Emp(generateEmployeeId,"Ghazi Samer","Adminstration","senior","",calculateSalary("Senior"));
+let Lana=new Emp(generateEmployeeId,"Lana Ali","Finance","Senior","",calculateSalary("Senior"));
+let Tamara=new Emp(generateEmployeeId,"Tamara Ayoub","Marketing","senior","",calculateSalary("Senior"));
+let Safi =new Emp(generateEmployeeId,"Safi Waleed","Administration","Mid-Senior","",calculateSalary("Midsenior"));
+ let omar=new Emp(generateEmployeeId,"Omar Zaid","Development","Senior","",calculateSalary("Senior"));           
+ let Rana=new Emp(generateEmployeeId,"Rana Saleh","Development","Junior","",calculateSalary("Junior"));
+ let Hadi=new Emp(generateEmployeeId,"Hadi Ahmad","Finance","Mid-Senior","",calculateSalary("Midsenior"));
+
+
+  
+    let employees = [Ghazi, Lana, Tamara, Safi, omar, Rana, Hadi];
+    
+
+                                         Emp.prototype.renderEmployees = function() {
+      let employeeList = "";
+    
+      employeeList += "Employee name: " + this.FullName + "<br>";
+      employeeList += "Department: " + this.Departement + "<br>";
+      employeeList += "Employee salary: " + this.Salary + "<br><br>";
+    
+      document.getElementById("employee-list").innerHTML += employeeList;
+    };
+    
+    
+    
+    
+    
+    
+                                               for (let i = 0; i < employees.length; i++) {
+      employees[i].renderEmployees();
+    }
